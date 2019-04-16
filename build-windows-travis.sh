594e0b47958c534ea073e764e112516e43635a75
Uriel Corfa <korfuri@gmail.com>
2019-04-16 21:14:43 +0900

    Log PATH in 2 different places

--- build-windows-travis.sh
+++ build-windows-travis.sh
@@ -1,1 +1,1 @@
-export PATH=/cygdrive/c/tools/cygwin/bin:$PATH; echo "Make deps"; make deps LIB_EXTENSION=lib; echo "Make dist"; make dist LIB_EXTENSION=lib;
+export PATH=/cygdrive/c/tools/cygwin/bin:$PATH; echo "Path inside build-windows-travis.sh: $PATH"; echo "Make deps"; make deps LIB_EXTENSION=lib; echo "Make dist"; make dist LIB_EXTENSION=lib;
