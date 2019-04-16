7da810f688f6604cb28a811e8cd44a05d2cd2588
Uriel Corfa <korfuri@gmail.com>
2019-04-16 17:46:05 +0900

    Who needs line endings anyway

--- build-windows-travis.sh
+++ build-windows-travis.sh
@@ -1,7 +1,1 @@
-export PATH=/cygdrive/c/tools/cygwin/bin:$PATH
-
-echo "Make deps"
-make deps
-
-echo "Make dist"
-make dist
+export PATH=/cygdrive/c/tools/cygwin/bin:$PATH; echo "Make deps"; make deps; echo "Make dist"; make dist;
