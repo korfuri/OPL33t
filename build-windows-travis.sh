573c3987a102b80392e60b027be8fa6f01effe04
Uriel Corfa <korfuri@gmail.com>
2019-04-16 18:05:52 +0900

    Fix typo for lib extension variable

--- build-windows-travis.sh
+++ build-windows-travis.sh
@@ -1,1 +1,1 @@
-export PATH=/cygdrive/c/tools/cygwin/bin:$PATH; echo "Make deps"; make deps LIBS_EXTENSION=lib; echo "Make dist"; make dist LIBS_EXTENSION=lib;
+export PATH=/cygdrive/c/tools/cygwin/bin:$PATH; echo "Make deps"; make deps LIB_EXTENSION=lib; echo "Make dist"; make dist LIB_EXTENSION=lib;
